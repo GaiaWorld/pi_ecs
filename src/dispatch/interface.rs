0dd8eadbfbd8966e0910ad9fda8dbd5bacee3c66
moyy <moyy@qq.com>
2022-02-16 11:43:33 +0800

    feat: 派发器 注释 和 接口类型 后置

--- src/dispatch/interface.rs
+++ src/dispatch/interface.rs
@@ -1,34 +1,52 @@
-use std::{io::Result, collections::HashSet};
 use std::sync::Arc;
+use std::{collections::HashSet, io::Result};
 
 use async_graph::{async_graph, Runnble, Runner};
 use futures::future::BoxFuture;
 use graph::{DirectedGraph, DirectedGraphNode, NGraph, NGraphBuilder};
 use r#async::rt::{AsyncRuntime, AsyncTaskPool, AsyncTaskPoolExt};
 
+pub trait Arrange {
+    fn arrang(&self) -> Option<GraphNode>;
+}
+
+/// Stage 是 由 可执行节点 组成的 图
+type Stage = Arc<NGraph<usize, ExecNode>>;
+
+/// 派发器 接口
 pub trait Dispatcher {
+    /// 只有 run 方法
     fn run(&self);
 }
 
-pub struct SingleDispatcher<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> {
-    vec: Arc<Vec<Arc<NGraph<usize, ExecNode>>>>,
+/// 串行 派发器
+pub struct SingleDispatcher<P>
+where
+    P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>,
+{
+    /// 异步运行时
     rt: AsyncRuntime<(), P>,
+    /// 派发器 包含 一组 Stage
+    vec: Arc<Vec<Stage>>,
 }
 
-impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> SingleDispatcher<P> {
-    pub fn new<T: Arrange>(vec: Vec<Arc<NGraph<usize, ExecNode>>>, arrange: &T, rt: AsyncRuntime<(), P>) -> Self {
-		let mut v1 = Vec::new();
-		for i in vec.into_iter() {
-			v1.push(i);
+impl<P> SingleDispatcher<P>
+where
+    P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>,
+{
+    pub fn new<T: Arrange>(vec: Vec<Stage>, arrange: &T, rt: AsyncRuntime<(), P>) -> Self {
+        let mut v1 = Vec::new();
+        for i in vec.into_iter() {
+            v1.push(i);
 
-			// arrange node
-			if let Some(node) = arrange.arrang() {
-				let mut stage = StageBuilder::new();
-				stage.add_node(node);
-				
-				v1.push(Arc::new(stage.build()))
-			}
-		}
+            // arrange node
+            if let Some(node) = arrange.arrang() {
+                let mut stage = StageBuilder::new();
+                stage.add_node(node);
+
+                v1.push(Arc::new(stage.build()))
+            }
+        }
         SingleDispatcher {
             vec: Arc::new(v1),
             rt,
@@ -37,7 +55,7 @@ impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> SingleDispatcher<P> 
 
     /// 执行指定阶段的指定节点
     pub fn exec(
-        vec: Arc<Vec<Arc<NGraph<usize, ExecNode>>>>,
+        vec: Arc<Vec<Stage>>,
         rt: AsyncRuntime<(), P>,
         mut stage_index: usize,
         mut node_index: usize,
@@ -46,12 +64,11 @@ impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> SingleDispatcher<P> 
             let g = &vec[stage_index];
             let arr = g.topological_sort();
             if node_index >= arr.len() {
-
-				// stage结束，apply
-				for i in 0..arr.len() {
-					let node = g.get(&arr[i]).unwrap().value();
-					node.apply();
-				}
+                // stage结束，apply
+                for i in 0..arr.len() {
+                    let node = g.get(&arr[i]).unwrap().value();
+                    node.apply();
+                }
                 stage_index += 1;
                 node_index = 0;
                 continue;
@@ -59,41 +76,58 @@ impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> SingleDispatcher<P> 
             let node = g.get(&arr[node_index]).unwrap().value();
             node_index += 1;
             match node.is_sync() {
-                Some(sync) => if sync {
-                    node.get_sync().run();
-                }else{
-                    let f = node.get_async();
-                    let vec1= vec.clone();
-                    let rt1= rt.clone();
-                    rt.spawn(rt.alloc(), async move {
-                        let _ = f.await;
-                        // println!("ok");
-                        SingleDispatcher::exec(vec1, rt1, stage_index, node_index + 1);
-                    }).unwrap();
-                },
+                Some(sync) => {
+                    if sync {
+                        node.get_sync().run();
+                    } else {
+                        let f = node.get_async();
+                        let vec1 = vec.clone();
+                        let rt1 = rt.clone();
+                        rt.spawn(rt.alloc(), async move {
+                            let _ = f.await;
+                            // println!("ok");
+                            SingleDispatcher::exec(vec1, rt1, stage_index, node_index + 1);
+                        })
+                        .unwrap();
+                    }
+                }
                 None => (),
             }
         }
     }
 }
-impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>> Dispatcher for SingleDispatcher<P> {
+impl<P> Dispatcher for SingleDispatcher<P>
+where
+    P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>,
+{
     /// 同步节点自己执行， 如果有异步节点，则用单线程运行时执行
     fn run(&self) {
         Self::exec(self.vec.clone(), self.rt.clone(), 0, 0);
     }
 }
-pub struct MultiDispatcher<P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>>(
-    Arc<MultiInner<P1, P2>>,
-);
-impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>> MultiDispatcher<P, P2> {
+pub struct MultiDispatcher<P1, P2>(Arc<MultiInner<P1, P2>>)
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>;
+
+impl<P1, P2> MultiDispatcher<P1, P2>
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
     pub fn new(
-        vec: Vec<(Arc<NGraph<usize, ExecNode>>, Option<AsyncRuntime<(), P2>>)>,
-        multi: AsyncRuntime<(), P>,
+        vec: Vec<(Stage, Option<AsyncRuntime<(), P2>>)>,
+        multi: AsyncRuntime<(), P1>,
     ) -> Self {
         MultiDispatcher(Arc::new(MultiInner::new(vec, multi)))
     }
 }
-impl<P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>> Dispatcher for MultiDispatcher<P1, P2> {
+
+impl<
+        P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+        P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+    > Dispatcher for MultiDispatcher<P1, P2>
+{
     /// 根据阶段是单线程还是多线程，
     /// 如果多线程阶段，同步节点和异步节点，则用多线程运行时并行执行
     /// 如果单线程阶段，同步节点自己执行， 如果有异步节点，则用单线程运行时执行
@@ -103,24 +137,35 @@ impl<P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>, P2: AsyncTaskPoolE
         exec(c, 0)
     }
 }
-struct MultiInner<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>> {
-    vec: Vec<(Arc<NGraph<usize, ExecNode>>, Option<AsyncRuntime<(), P2>>)>,
-    multi: AsyncRuntime<(), P>,
+
+struct MultiInner<P1, P2>
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
+    vec: Vec<(Stage, Option<AsyncRuntime<(), P2>>)>,
+    multi: AsyncRuntime<(), P1>,
 }
-impl<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>> MultiInner<P, P2> {
+
+impl<P1, P2> MultiInner<P1, P2>
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
     pub fn new(
-        vec: Vec<(Arc<NGraph<usize, ExecNode>>, Option<AsyncRuntime<(), P2>>)>,
-        multi: AsyncRuntime<(), P>,
+        vec: Vec<(Stage, Option<AsyncRuntime<(), P2>>)>,
+        multi: AsyncRuntime<(), P1>,
     ) -> Self {
         MultiInner { vec, multi }
     }
 }
 
 /// 执行指定阶段
-fn exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>>(
-    d: Arc<MultiInner<P, P2>>,
-    stage_index: usize,
-) {
+fn exec<P1, P2>(d: Arc<MultiInner<P1, P2>>, stage_index: usize)
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
     if stage_index >= d.vec.len() {
         return;
     }
@@ -132,29 +177,31 @@ fn exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPool
     }
 }
 
-/// 单线程执行, 尽量本线程运行， 遇到异步节点则用单线程运行时运行
-fn single_exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>>(
-    d: Arc<MultiInner<P, P2>>,
+/// 单线程执行, 尽量本线程运行，遇到异步节点则用单线程运行时运行
+fn single_exec<P1, P2>(
+    d: Arc<MultiInner<P1, P2>>,
     stage_index: usize,
     mut node_index: usize,
     single: AsyncRuntime<(), P2>,
-) {
+) where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
     let g = &d.vec[stage_index].0;
-    let single1= single.clone();
+    let single1 = single.clone();
     loop {
-		let arr = g.topological_sort();
+        let arr = g.topological_sort();
         if node_index >= g.node_count() {
-
-			// stage结束，apply
-			for i in 0..g.node_count() {
-				let node = g.get(&arr[i]).unwrap().value();
-				node.apply();
-			}
+            // stage结束，apply
+            for i in 0..g.node_count() {
+                let node = g.get(&arr[i]).unwrap().value();
+                node.apply();
+            }
 
             // 本阶段执行完毕，执行下一阶段
             return exec(d, stage_index + 1);
         }
-        
+
         let node = g.get(&arr[node_index]).unwrap().value();
         node_index += 1;
         match node.is_sync() {
@@ -163,10 +210,12 @@ fn single_exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncT
                     if stage_index > 0 && node_index == 1 {
                         let f = node.get_sync();
                         let d1 = d.clone();
-                        single1.spawn(single1.alloc(), async move {
-                            f.run();
-                            single_exec(d1, stage_index, node_index, single);
-                        }).unwrap();
+                        single1
+                            .spawn(single1.alloc(), async move {
+                                f.run();
+                                single_exec(d1, stage_index, node_index, single);
+                            })
+                            .unwrap();
                         return;
                     }
                     // 如果是最开始的阶段， 或者非起始节点，则立即同步执行
@@ -174,10 +223,12 @@ fn single_exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncT
                 } else {
                     let f = node.get_async();
                     let d1 = d.clone();
-                    single1.spawn(single1.alloc(), async move {
-                        let _ = f.await;
-                        single_exec(d1, stage_index, node_index, single);
-                    }).unwrap();
+                    single1
+                        .spawn(single1.alloc(), async move {
+                            let _ = f.await;
+                            single_exec(d1, stage_index, node_index, single);
+                        })
+                        .unwrap();
                     return;
                 }
             }
@@ -185,42 +236,61 @@ fn single_exec<P: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P>, P2: AsyncT
         }
     }
 }
+
 /// 多线程执行
-fn multi_exec<P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>, P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>>(
-    d: Arc<MultiInner<P1, P2>>,
-    stage_index: usize,
-) {
+fn multi_exec<P1, P2>(d: Arc<MultiInner<P1, P2>>, stage_index: usize)
+where
+    P1: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P1>,
+    P2: AsyncTaskPoolExt<()> + AsyncTaskPool<(), Pool = P2>,
+{
     let d1 = d.clone();
-    d.multi.spawn(d.multi.alloc(), async move {
-        let g = &d1.vec[stage_index].0;
-        let r = async_graph(d1.multi.clone(), g.clone()).await;
-        if r.is_ok() {
+    d.multi
+        .spawn(d.multi.alloc(), async move {
+            let g = &d1.vec[stage_index].0;
+            let r = async_graph(d1.multi.clone(), g.clone()).await;
+            if r.is_ok() {
+                // stage结束，apply
+                let arr = g.topological_sort();
+                for i in 0..g.node_count() {
+                    let node = g.get(&arr[i]).unwrap().value();
+                    node.apply();
+                }
 
-			// stage结束，apply
-			let arr = g.topological_sort();
-			for i in 0..g.node_count() {
-				let node = g.get(&arr[i]).unwrap().value();
-				node.apply();
-			}
-
-            exec(d1, stage_index + 1);
-        }
-    }).unwrap();
+                exec(d1, stage_index + 1);
+            }
+        })
+        .unwrap();
 }
+
+/// 图 的 节点
 pub struct GraphNode {
+    // 节点id，每个节点有 独一无二的 id
     pub(crate) id: usize,
+    // 节点的输入，意义是 它 依赖 的 节点，决定 执行关系
     pub(crate) reads: Vec<usize>,
+    // 节点的输出，意义是 依赖 它 的 节点，决定 执行关系
     pub(crate) writes: Vec<usize>,
+    // 执行节点
     pub(crate) node: ExecNode,
 }
 
+/// 操作
 pub trait Operate {
-	type R;
-	fn run(&self) -> Self::R;
-	fn apply(&self);
+    /// 返回类型
+    type R;
+
+    /// 执行
+    /// 执行结果，会缓冲到 内部，等当前Stage全部执行结束后，再统一调用apply，刷新到world上
+    fn run(&self) -> Self::R;
+
+    /// 应用
+    /// 在该stage所有的system run 结束之后 执行
+    /// 扫描所有的system，将当前缓冲的数据 刷新到 world 上
+    fn apply(&self);
 }
 
-pub struct Run(pub(crate) Arc<dyn Operate<R=()>>);
+/// 对操作的 封装
+pub struct Run(pub(crate) Arc<dyn Operate<R = ()>>);
 unsafe impl Send for Run {}
 unsafe impl Sync for Run {}
 
@@ -230,13 +300,19 @@ impl Runner for Run {
     }
 }
 
+/// 执行节点
 pub enum ExecNode {
+    /// 不执行任何操作
     None,
+    /// 同步函数
     Sync(Run),
-    Async(Box<dyn Operate<R=BoxFuture<'static, Result<()>>> + 'static + Send + Sync>),
+    /// 异步函数
+    Async(Box<dyn Operate<R = BoxFuture<'static, Result<()>>> + 'static + Send + Sync>),
 }
+
 impl Runnble for ExecNode {
     type R = Run;
+
     fn is_sync(&self) -> Option<bool> {
         match self {
             ExecNode::None => None,
@@ -244,6 +320,7 @@ impl Runnble for ExecNode {
             _ => Some(false),
         }
     }
+
     /// 获得需要执行的同步函数
     fn get_sync(&self) -> Run {
         match self {
@@ -251,6 +328,7 @@ impl Runnble for ExecNode {
             _ => panic!(),
         }
     }
+
     /// 获得需要执行的异步块
     fn get_async(&self) -> BoxFuture<'static, Result<()>> {
         match self {
@@ -261,22 +339,27 @@ impl Runnble for ExecNode {
 }
 
 impl ExecNode {
-	fn apply(&self) {
-		match self {
-			ExecNode::Sync(f) => f.0.apply(),
+    fn apply(&self) {
+        match self {
+            ExecNode::Sync(f) => f.0.apply(),
             ExecNode::Async(f) => f.apply(),
             _ => (),
         };
-	}
+    }
 }
 
+/// 阶段构造器
 pub struct StageBuilder {
+    // 所有的节点 id
     components: HashSet<usize>,
+    // 节点
     systems: Vec<GraphNode>,
+    // 边，(输入节点id, 输出节点id)
     edges: Vec<(usize, usize)>,
 }
 
 impl StageBuilder {
+    /// 创建
     pub fn new() -> Self {
         StageBuilder {
             components: HashSet::default(),
@@ -285,40 +368,58 @@ impl StageBuilder {
         }
     }
 
+    /// 加入节点
     pub fn add_node<T: Into<GraphNode>>(&mut self, node: T) -> &mut Self {
         let node = node.into();
 
         for k in &node.reads {
             self.components.insert(*k);
+
+            // 边: 输入 --> 该节点
             self.edges.push((*k, node.id));
         }
+
         for k in &node.writes {
             self.components.insert(*k);
+
+            // 边: 该节点 --> 输出
             self.edges.push((node.id, *k));
         }
+
+        // 加入 System
         self.systems.push(node);
+
         self
     }
 
+    /// 显示指定 节点的依赖 关系
     pub fn order(mut self, before: usize, after: usize) -> Self {
+        // 添加边: before --> after
         self.edges.push((before, after));
+
         self
     }
+
+    /// 构建 拓扑 序
     pub fn build(self) -> NGraph<usize, ExecNode> {
+        // Stages --> NGraph
         let mut builder = NGraphBuilder::new();
+
         for id in self.components {
+            // 每个 Component 都是一个节点
             builder = builder.node(id, ExecNode::None);
         }
+
         for n in self.systems {
+            // 每个 System 都是 一个 可执行节点
             builder = builder.node(n.id, n.node);
         }
+
         for n in self.edges {
+            // 边 对应 Graph 的 边
             builder = builder.edge(n.0, n.1);
         }
+
         builder.build().unwrap()
     }
-}
-
-pub trait Arrange {
-	fn arrang(&self) -> Option<GraphNode>;
 }
--- src/dispatch/nodes.rs
+++ src/dispatch/nodes.rs
@@ -1,154 +1,153 @@
-use std::{sync::Arc};
-use std::io::Result;
-
-use share::cell::TrustCell;
-use futures::future::BoxFuture;
-
+use super::interface::Run;
+use crate::dispatch::interface::{Arrange, ExecNode, GraphNode, Operate};
 use crate::storage::Local;
 use crate::{
-	world::World,
-	sys::{
-		system::{
-			func_sys::{
-				FunctionSystem,
-				SystemParamFunction,
-			}, 
-			System
-		}, 
-		param::SystemParam
-	},
+    sys::{
+        param::SystemParam,
+        system::{
+            func_sys::{FunctionSystem, SystemParamFunction},
+            System,
+        },
+    },
+    world::World,
 };
+use futures::future::BoxFuture;
+use share::cell::TrustCell;
+use std::io::Result;
+use std::sync::Arc;
 
-use crate::dispatch::interface::{GraphNode, ExecNode, Arrange, Operate};
+pub struct SyncRun<Param: SystemParam + 'static, F>(
+    pub(crate) TrustCell<FunctionSystem<(), (), Param, (), F>>,
+);
 
-use super::interface::Run;
+pub struct AsyncRun<Param: SystemParam + 'static, F>(
+    pub(crate) TrustCell<FunctionSystem<(), BoxFuture<'static, Result<()>>, Param, (), F>>,
+);
 
+impl<Param: SystemParam + 'static, F> Operate for SyncRun<Param, F>
+where
+    F: Send + Sync + SystemParamFunction<(), (), Param, ()>,
+{
+    type R = ();
 
-pub struct SyncRun<Param: SystemParam + 'static, F>(pub(crate) TrustCell<
-	FunctionSystem<(), (), Param, (), F>
->);
+    fn run(&self) {
+        self.0.borrow_mut().run(());
+    }
 
-pub struct AsyncRun<Param: SystemParam + 'static, F>(pub(crate) TrustCell<
-	FunctionSystem<(), BoxFuture<'static, Result<()> >, Param, (), F>
->);
-
-impl<Param: SystemParam + 'static, F> Operate for SyncRun<Param, F> where 
-	F: Send + Sync + SystemParamFunction<(), (), Param, ()>{
-
-	type R=();
-	fn run(&self) {
-		self.0.borrow_mut().run(());
-	}
-	fn apply(&self) {
-		self.0.borrow_mut().apply_buffers();
-	}
+    fn apply(&self) {
+        self.0.borrow_mut().apply_buffers();
+    }
 }
 
-impl<Param: SystemParam + 'static, F> Operate for AsyncRun<Param, F> where 
-	F: Send + Sync + SystemParamFunction<(), BoxFuture<'static, Result<()>>, Param, ()> {
+impl<Param: SystemParam + 'static, F> Operate for AsyncRun<Param, F>
+where
+    F: Send + Sync + SystemParamFunction<(), BoxFuture<'static, Result<()>>, Param, ()>,
+{
+    type R = BoxFuture<'static, Result<()>>;
 
-	type R= BoxFuture<'static, Result<()>>;
-
-	fn run(&self) -> BoxFuture<'static, Result<()>> {
-		self.0.borrow_mut().run(())
-	}
-	fn apply(&self) {
-		self.0.borrow_mut().apply_buffers();
-	}
+    fn run(&self) -> BoxFuture<'static, Result<()>> {
+        self.0.borrow_mut().run(())
+    }
+    fn apply(&self) {
+        self.0.borrow_mut().apply_buffers();
+    }
 }
 
+impl<Param: SystemParam + 'static, F> Into<GraphNode> for FunctionSystem<(), (), Param, (), F>
+where
+    F: Send + Sync + SystemParamFunction<(), (), Param, ()>,
+{
+    default fn into(self) -> GraphNode {
+        let id = self.id();
+        let component_access = self.archetype_component_access();
+        let access = component_access.clone();
+        let mut reads = component_access.get_reads_and_writes().clone();
+        reads.symmetric_difference_with(access.get_writes());
+        let mut writes = component_access.get_writes().clone();
 
-impl<Param: SystemParam + 'static, F> Into<GraphNode> for FunctionSystem<(), (), Param, (), F> where 
-	F: Send + Sync + SystemParamFunction<(), (), Param, ()> {
+        let w = self.world();
 
-	default fn into(self) -> GraphNode {
-		let id = self.id();
-		let component_access = self.archetype_component_access();
-		let access = component_access.clone();
-		let mut reads = component_access.get_reads_and_writes().clone();
-		reads.symmetric_difference_with(access.get_writes());
-		let mut writes = component_access.get_writes().clone();
+        for write in component_access.get_writes().ones().into_iter() {
+            let v = w.listener_access.get(Local::new(write));
+            if let Some(r) = v {
+                for access1 in r.iter() {
+                    // 有相同元素
+                    if component_access
+                        .get_writes()
+                        .is_disjoint(access1.get_writes())
+                    {
+                        panic!("write conflict");
+                    }
 
-		let w = self.world();
+                    // 扩充读
+                    let mut r = access1.get_reads_and_writes().clone();
+                    r.symmetric_difference_with(access1.get_writes());
+                    reads.union_with(&r);
 
-		for write in component_access.get_writes().ones().into_iter() {
-			let v = w.listener_access.get(Local::new(write));
-			if let Some(r) = v {
-				for access1 in r.iter() {
-					// 有相同元素
-					if component_access.get_writes().is_disjoint(access1.get_writes()) {
-						panic!("write conflict");
-					}
+                    writes.union_with(access1.get_writes());
+                }
+            }
+        }
 
-					// 扩充读
-					let mut r = access1.get_reads_and_writes().clone();
-					r.symmetric_difference_with(access1.get_writes());
-					reads.union_with(&r);
-					
-					writes.union_with(access1.get_writes());
-				}
-			}
-		}
-
-
-		let reads = reads.ones().into_iter().collect();
-		let writes = writes.ones().into_iter().collect();
-		let sys = TrustCell::new(self);
-		GraphNode{
-			id: id.id(),
-			reads,
-			writes,
-			node: ExecNode::Sync(Run(Arc::new(SyncRun(sys))))
-		}
-	}
+        let reads = reads.ones().into_iter().collect();
+        let writes = writes.ones().into_iter().collect();
+        let sys = TrustCell::new(self);
+        GraphNode {
+            id: id.id(),
+            reads,
+            writes,
+            node: ExecNode::Sync(Run(Arc::new(SyncRun(sys)))),
+        }
+    }
 }
 
-impl<Param: SystemParam + 'static, F> Into<GraphNode> for FunctionSystem<(), BoxFuture<'static, Result<()> >, Param, (), F> where 
-	F: Send + Sync + SystemParamFunction<(), BoxFuture<'static, Result<()>>, Param, ()> {
-	fn into(self) -> GraphNode {
-		let id = self.id();
-		let component_access = self.archetype_component_access();
-		
-		let reads = component_access
-								.get_reads_and_writes()
-								.difference(component_access.get_writes())
-								.collect();
-		let writes = component_access.get_writes().ones().into_iter().collect();
-		let sys = TrustCell::new(self);
-		GraphNode{
-			id: id.id(),
-			reads: reads,
-			writes: writes,
-			node: ExecNode::Async(Box::new(AsyncRun(sys)))
-		}
-	}
+impl<Param: SystemParam + 'static, F> Into<GraphNode>
+    for FunctionSystem<(), BoxFuture<'static, Result<()>>, Param, (), F>
+where
+    F: Send + Sync + SystemParamFunction<(), BoxFuture<'static, Result<()>>, Param, ()>,
+{
+    fn into(self) -> GraphNode {
+        let id = self.id();
+        let component_access = self.archetype_component_access();
+
+        let reads = component_access
+            .get_reads_and_writes()
+            .difference(component_access.get_writes())
+            .collect();
+        let writes = component_access.get_writes().ones().into_iter().collect();
+        let sys = TrustCell::new(self);
+        GraphNode {
+            id: id.id(),
+            reads: reads,
+            writes: writes,
+            node: ExecNode::Async(Box::new(AsyncRun(sys))),
+        }
+    }
 }
 
 impl Arrange for World {
-	fn arrang(&self) -> Option<GraphNode> {
-		let mut w = self.clone();
-		let id = w.archetype_component_grow();
-		let sys = move|| {
-			w.increment_change_tick();
-		};
-		Some(GraphNode {
-			id,
-			reads: Vec::new(),
-			writes: Vec::new(),
-			node: ExecNode::Sync(Run(Arc::new(FnSys(Box::new(sys)))))
-		})
-	}
+    fn arrang(&self) -> Option<GraphNode> {
+        let mut w = self.clone();
+        let id = w.archetype_component_grow();
+        let sys = move || {
+            w.increment_change_tick();
+        };
+        Some(GraphNode {
+            id,
+            reads: Vec::new(),
+            writes: Vec::new(),
+            node: ExecNode::Sync(Run(Arc::new(FnSys(Box::new(sys))))),
+        })
+    }
 }
 
 pub struct FnSys(pub(crate) Box<dyn Fn()>);
 
-impl Operate for FnSys  {
+impl Operate for FnSys {
+    type R = ();
 
-	type R = ();
-
-	fn run(&self) {
-		self.0();
-	}
-	fn apply(&self) {
-	}
+    fn run(&self) {
+        self.0();
+    }
+    fn apply(&self) {}
 }
