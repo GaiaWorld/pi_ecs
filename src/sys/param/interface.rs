800eea3c8e1cba1c198a76746c6e393baa3a5694
moyy <412001799@qq.com>
2022-03-29 10:36:05 +0800

    feat: 添加类型 SystemParamItem

--- src/sys/param/interface.rs
+++ src/sys/param/interface.rs
@@ -1,12 +1,14 @@
-use pi_ecs_macros::all_tuples;
-use crate::world::World;
 use crate::component::ComponentId;
-use crate::query::{FilteredAccessSet, FilteredAccess};
+use crate::query::{FilteredAccess, FilteredAccessSet};
 use crate::sys::system::interface::SystemState;
+use crate::world::World;
+use pi_ecs_macros::all_tuples;
 
 pub trait SystemParam: Sized {
     type Fetch: for<'a> SystemParamFetch<'a>;
 }
+
+pub type SystemParamItem<'a, P> = <<P as SystemParam>::Fetch as SystemParamFetch<'a>>::Item;
 
 pub trait SystemParamFetch<'a>: SystemParamState {
     type Item;
@@ -24,14 +26,14 @@ pub trait SystemParamFetch<'a>: SystemParamState {
 
 pub unsafe trait SystemParamState: Send + Sync + 'static {
     type Config: Send + Sync;
-	/// 系统状态初始化
-	/// 通常做以下事情：
-	/// * 检查当前参数是否与当前系统中的数据访问是否冲突（同一系统，不能同时读写相同数据）
-	/// * 初始化该该参数在整个系统生命周期内不会改变的其他状态（每个系统参数会有各自不同的状态，根据自身需求初始化）
+    /// 系统状态初始化
+    /// 通常做以下事情：
+    /// * 检查当前参数是否与当前系统中的数据访问是否冲突（同一系统，不能同时读写相同数据）
+    /// * 初始化该该参数在整个系统生命周期内不会改变的其他状态（每个系统参数会有各自不同的状态，根据自身需求初始化）
     fn init(world: &mut World, system_state: &mut SystemState, config: Self::Config) -> Self;
 
-	/// 每个stage运行结束后，该参数应该被调用
-	/// 通常用于刷新、整理前一阶段的数据（如延迟的指令操作需要flush、脏列表需要清理）}
+    /// 每个stage运行结束后，该参数应该被调用
+    /// 通常用于刷新、整理前一阶段的数据（如延迟的指令操作需要flush、脏列表需要清理）}
     #[inline]
     fn apply(&mut self, _world: &mut World) {}
 
@@ -84,7 +86,6 @@ macro_rules! impl_system_param_tuple {
     };
 }
 
-
 all_tuples!(impl_system_param_tuple, 0, 16, P);
 
 pub(crate) fn assert_component_access_compatibility(
@@ -107,4 +108,3 @@ pub(crate) fn assert_component_access_compatibility(
     panic!("Query<{}, {}> in system {} accesses component(s) {} in a way that conflicts with a previous system parameter. Allowing this would break Rust's mutability rules. Consider merging conflicting Queries into a QuerySet.",
                 query_type, filter_type, system_name, accesses);
 }
-
